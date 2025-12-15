248852a5e14cb420280ae31b57a4c6323a8f9984
Jorge Matos <jisapjisap@gmail.com>
2025-12-15 16:40:56 +0100

    feat: add new Hero section component and initial global styling.

--- app/Sections/Hero/page.tsx
+++ app/Sections/Hero/page.tsx
@@ -8,33 +8,28 @@ const Hero = () => {
   return (
     <div className="Hero section-bg px-[8%] lg:px-[12%]">
       <div className="w-full flex flex-col lg:flex-row justify-between items-center gap-5">
-        <div className="w-full lg:w-1/2">
+        <div className="w-full lg:w-1/2 text-center lg:text-left">
           <h4 className="text-secondary Inter uppercase text-xl">
             From Concept to Creation
           </h4>
 
-          <h1 className="text-7xl lg:text-8xl font-bold Syne my-4 text-gray-300 ">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold Syne my-4 text-gray-300 ">
             Precision 3D Printing Solutions
           </h1>
 
-          <div className="flex items-center gap-5 mt-5">
-            <button className="btn px-8 py-2 border border-primary bg-primary rounded-full text-xl hover:bg-transparent cursor-pointer transition-all duration-300 hover:text-primary">
+          <div className="flex items-center justify-center lg:justify-start gap-5 mt-5">
+            <button className="btn px-4 sm:px-6 md:px-8 py-2 border border-primary bg-primary rounded-full text-base sm:text-lg md:text-xl hover:bg-transparent cursor-pointer transition-all duration-300 hover:text-primary">
               Explore More
             </button>
 
-            <button className="btn px-8 py-2 text-white border border-primary bg-transparent rounded-full text-xl hover:bg-primary cursor-pointer transition-all duration-300 hover:text-body">
+            <button className="btn px-4 sm:px-6 md:px-8 py-2 text-white border border-primary bg-transparent rounded-full text-base sm:text-lg md:text-xl hover:bg-primary cursor-pointer transition-all duration-300 hover:text-body">
               <i className="bi bi-play-fill"></i>Video Production
             </button>
           </div>
 
         </div>
 
-        <div className="w-full lg:w-1/2">
-          <p className="text-gray-300 Inter text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium accusamus
-            voluptates odit laborum quibusdam mollitia dignissimos est nostrum minima.
-          </p>
-
+        <div className="w-full lg:w-1/2 flex flex-col items-center">
           <div className="hero-image">
             <Image
               src={HeroImg}
@@ -43,6 +38,11 @@ const Hero = () => {
               width={700}
             />
           </div>
+
+          <p className="text-gray-300 Inter text-base md:text-lg text-center mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium accusamus
+            voluptates odit laborum quibusdam mollitia dignissimos est nostrum minima.
+          </p>
         </div>
       </div>
     </div>
